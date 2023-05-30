c5d1ef0ad5e4f4b1a7723a5b3f42975824ecc121
sadamm27 <sadammochammad409@gmail.com>
2023-05-30 17:04:46 +0700

    todo list using js

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const bodyParser = require("body-parser");
+const date = require (__dirname + "/date.js")
+
+const items = ["Buy Food","Eat Food","Cook Food"];
+const workItems = [];
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    const day = date.getDate();
+
+    res.render("list", {listTitle: day, newListItems: items});
+    
+  });
+  
+  app.post("/", (req, res) => {
+    const item = req.body.newItem;
+    if (req.body.list === "Work") {
+      workItems.push(item);
+      res.redirect("/work");
+    } else {
+      items.push(item);
+      res.redirect("/");
+      
+    }
+})
+  app.get("/work", (req, res) => {
+    res.render("list", {listTitle: "Work List", newListItems: workItems})
+  })
+
+  app.post("/work", (req, res) => {
+    const item = req.body.newItem;
+    workItems.push(item);
+    res.redirect("/");
+  })
+
+  app.get("/about", (req, res) => {
+    res.render("about");
+  })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
